c33b39dd3f86b8e3afc203facb8a3ec759412bee
Annie Carmichael <42788203+anniecarm@users.noreply.github.com>
2019-12-09 11:24:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.thought1').delay(2000).fadeIn(4000).removeClass('.hidden');
+    $('.thought2').delay(4000).fadeIn(4000).removeClass('.hidden');
+    $('.thought3').delay(6000).fadeIn(4000).removeClass('.hidden');
+    $('.thought4').delay(8000).fadeIn(4000).removeClass('.hidden');
+    $('.thought5').delay(10000).fadeIn(4000).removeClass('.hidden');
+    $('.thought6').delay(12000).fadeIn(4000).removeClass('.hidden');
+});
+
+
+$(document).ready(function () {
+    $('p:first').fadeIn(4000);
+});
